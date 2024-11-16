b187d39ba46f68af478d465eb07676a255bf3825
hienjoy <wisdom16no@naver.com>
2024-11-17 00:26:06 +0900

    feat: Banner 요소 추가

--- src/pages/mainPage/components/banner/Banner.jsx
+++ src/pages/mainPage/components/banner/Banner.jsx
@@ -1,59 +1,81 @@
 import styled from "styled-components";
-import mockData from "./mockData";
-import BannerCard from "./BannerCard";
+import BannerCarousel from "./BannerCarousel";
+import SearchIcon from "../../assets/SearchIcon.png";
 
 const BannerSection = styled.section`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    height: 300px;
-    gap: 30px;
-    background-color: #f0f0f0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  gap: 50px;
+  background-color: #f0f0f0;
 `;
 
 const BannerContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background-color: #f0f0f0;
-    color: #333;
-    font-weight: bold;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #333;
+  font-weight: bold;
+  text-align: center;
 `;
 
 const SearchContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: end;
-    align-items: start;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: start;
 
-    height: 100%;
-    gap: 30px;
-    padding: 40px 0;
+  height: 100%;
+  gap: 30px;
+  padding: 40px 0;
 `;
 
+const SearchTitle = styled.h2`
+  font-size: 40px;
+  font-weight: 600;
+`;
+
+const SearchBar = styled.div`
+  width: 466px;
+  height: 55px;
+  background: white;
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+  border: 2px solid #00a8cc;
+  border-radius: 50px;
+  margin-top: 10px;
+`;
+
+const SearchTextField = styled.input`
+  width: 350px;
+  border: none;
+  outline: none;
+  text-align: center;
+`;
 
 export default function Banner() {
-    return (
-        <BannerSection>
-            <BannerContainer>
-                {/* 나중에 이 컨테이너를 상하로 움직이는 캐러셀로 바꿔주세요! */}
-                <BannerCard
-                    title={mockData.title}
-                    subtitle={mockData.subtitle}
-                    description={mockData.description}
-                    logo={mockData.logo}
-                />
-            </BannerContainer>
-            <SearchContainer >
-                <h2>이런 프로젝트는 어때요?</h2>
-                <div>
-                    <p>다른 프로젝트를 찾으시나요?</p>
-                    <input type="text" placeholder="어쩌구 저쩌구를 검색해보세요!" />
-                </div>
-            </SearchContainer>
-        </BannerSection>
-    );
-};
+  return (
+    <BannerSection>
+      <BannerContainer>
+        <BannerCarousel />
+      </BannerContainer>
+      <SearchContainer>
+        <SearchTitle>이런 프로젝트는 어때요?</SearchTitle>
+        <div>
+          <p>다른 프로젝트를 찾으시나요?</p>
+          <SearchBar>
+            <img src={SearchIcon} alt="돋보기 아이콘" />
+            <SearchTextField
+              type="text"
+              placeholder="원하시는 프로젝트를 검색해보세요!"
+            />
+          </SearchBar>
+        </div>
+      </SearchContainer>
+    </BannerSection>
+  );
+}
--- src/pages/mainPage/components/banner/BannerCard.styled.js
+++ src/pages/mainPage/components/banner/BannerCard.styled.js
@@ -1,46 +1,48 @@
 import styled from "styled-components";
 
 export const BannerContainer = styled.div`
-    background-color: #FFD600;
-    padding: 20px;
-    border-radius: 15px;
-    text-align: center;
-    color: #000;
+  background-color: #ffd600;
+  padding: 20px;
+  border-radius: 15px;
+  text-align: center;
+  color: #000;
+  height: 300px;
+  width: 480px;
 `;
 
 export const Title = styled.h2`
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
 `;
 
 export const LogoContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 15px;
 `;
 
 export const Logo = styled.img`
-    width: 50px;
-    height: 50px;
-    margin-right: 15px;
+  width: 120px;
+  height: 120px;
+  margin-right: 15px;
 `;
 
 export const Subtitle = styled.p`
-    font-size: 16px;
-    font-weight: normal;
+  font-size: 16px;
+  font-weight: normal;
 `;
 
 export const Description = styled.p`
-    font-size: 14px;
-    line-height: 1.5;
-    margin-bottom: 20px;
+  font-size: 14px;
+  line-height: 1.5;
+  margin-bottom: 20px;
 `;
 
 export const ProjectName = styled.h3`
-    font-size: 18px;
-    font-weight: bold;
-    text-decoration: underline;
-    margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  text-decoration: underline;
+  margin-top: 10px;
 `;
