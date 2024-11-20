5d1b5508396a9300d872e7b67af43e0c3814bd4d
hienjoy <wisdom16no@naver.com>
2024-11-20 18:48:43 +0900

    feat: 2단계 컴포넌트 추가

--- src/pages/clubRegisterPage/components/FormStepTwo.jsx
+++ src/pages/clubRegisterPage/components/FormStepTwo.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
-import { Button } from "../ClubRegisterPage.styled";
+import { useNavigate } from "react-router-dom";
 import ClubThumbnail from "./ClubThumbnail";
 import ClubContent from "./ClubContent";
 import ClubCriteria from "./ClubCriteria";
+import SubmitModal from "./SubmitModal";
+import {
+  FormListContainer,
+  ListDescriptionContainer,
+  ButtonContainer,
+  Button,
+} from "../ClubRegisterPage.styled";
 
 const FormStepTwo = ({ changeStep, formData, setFormData }) => {
   const [thumbnail, setThumbnail] = useState(formData.thumbnail);
   const [content, setContent] = useState(formData.content);
   const [criteria, setCriteria] = useState(formData.criteria);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setThumbnail(formData.thumbnail);
@@ -41,6 +50,24 @@ const FormStepTwo = ({ changeStep, formData, setFormData }) => {
     }));
   };
 
+  const handleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleSubmit = () => {
+    console.log("입력 폼 데이터:", formData);
+    navigate("/club/register/done");
+  };
+
+  const isComplete = () => {
+    return Object.entries(formData).every(([key, value]) => {
+      if (key === "content") {
+        return Object.values(value).every((list) => list);
+      }
+      return value;
+    });
+  };
+
   return (
     <div>
       <ClubThumbnail
@@ -49,7 +76,26 @@ const FormStepTwo = ({ changeStep, formData, setFormData }) => {
       />
       <ClubContent onChange={onContentChange} initialContent={content} />
       <ClubCriteria onChange={onCriteriaChange} initialCriteria={criteria} />
-      <Button onClick={() => changeStep("1")}>이전 페이지</Button>
+      <FormListContainer>
+        <ListDescriptionContainer>
+          <ButtonContainer className="before">
+            <Button onClick={() => changeStep("1")}>이전 페이지</Button>
+          </ButtonContainer>
+        </ListDescriptionContainer>
+        <ButtonContainer>
+          <Button
+            className="submit-button"
+            onClick={handleModal}
+            disabled={!isComplete()}
+          >
+            제출하기
+          </Button>
+        </ButtonContainer>
+      </FormListContainer>
+
+      {isModalOpen && (
+        <SubmitModal onClose={handleModal} onSubmit={handleSubmit} />
+      )}
     </div>
   );
 };
