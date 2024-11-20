26de6871072d4831d878c2b1eb7c11a2db107bfc
Chaeri-1 <alfsdo03@hufs.ac.kr>
2024-11-21 01:28:43 +0900

    css: 구매관리 창

--- src/pages/myPage/routerPages/home/components/myBudgetInfo/MyBudgetInfo.jsx
+++ src/pages/myPage/routerPages/home/components/myBudgetInfo/MyBudgetInfo.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { BudgetBox, MyBudgetInfoContainer } from './MyBudgetInfo.styled';
+import { BudegtInfoBox, BudgetBox, MyBudgetInfoContainer, MyStateBox, MyStateLine } from './MyBudgetInfo.styled';
+import cash from '../../assets/mypage-cash-icon.svg'
+import coupon from '../../assets/mypage-coupon-icon.svg'
+import voucher from '../../assets/mypage-voucher-icon.svg'
 
 const MyBudgetInfo = () => {
     return (
         <MyBudgetInfoContainer>
-            <BudgetBox>캐시</BudgetBox>
-            <BudgetBox>쿠폰</BudgetBox>
-            <BudgetBox>이용권</BudgetBox>
-            <BudgetBox>머시기</BudgetBox>
+            <BudgetBox>
+                <img src={cash} alt="캐시 아이콘" />
+                <BudegtInfoBox>캐시<span>0</span></BudegtInfoBox>
+            </BudgetBox>
+            <BudgetBox>
+                <img src={coupon} alt="쿠폰 아이콘" />
+                <BudegtInfoBox>쿠폰<span>0</span></BudegtInfoBox>
+            </BudgetBox>
+            <BudgetBox>
+                <img src={voucher} alt="이용권 아이콘" />
+                <BudegtInfoBox>캐시<span>0</span></BudegtInfoBox>
+            </BudgetBox>
+            <MyStateBox>
+                <MyStateLine>참가중<span>4</span></MyStateLine>
+                <MyStateLine>모집중<span>1</span></MyStateLine>
+                <MyStateLine>구매 확정 대기<span>3</span></MyStateLine>
+            </MyStateBox>
         </MyBudgetInfoContainer>
     );
 };
--- src/pages/myPage/routerPages/home/components/myBudgetInfo/MyBudgetInfo.styled.js
+++ src/pages/myPage/routerPages/home/components/myBudgetInfo/MyBudgetInfo.styled.js
@@ -13,4 +13,45 @@ export const BudgetBox = styled.div`
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     background-color: #fff;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
 `;
+
+export const BudegtInfoBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: right;
+    height: 70px;
+    font-size: 18px;
+
+    span {
+        display: flex;
+        justify-content: right;
+        font-size: 24px;
+        font-weight: 800;
+    }
+`;
+
+export const MyStateBox = styled.div`
+    width: 100%;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const MyStateLine = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    font-size: 16px;
+
+    span {
+        font-weight: 700;
+    }
+`
