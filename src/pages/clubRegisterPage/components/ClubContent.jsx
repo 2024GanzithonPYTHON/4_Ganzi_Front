3ed9cf264da9d1d47e89d0112b3abea8a2442b31
hienjoy <wisdom16no@naver.com>
2024-11-20 18:46:05 +0900

    feat: 프로젝트 본문 입력 추가

--- src/pages/clubRegisterPage/components/ClubContent.jsx
+++ src/pages/clubRegisterPage/components/ClubContent.jsx
@@ -8,11 +8,12 @@ import {
   RequiredMarker,
   ListDescription,
   InputContainer,
+  InputContentContainer,
   TextArea,
 } from "../ClubRegisterPage.styled";
 
 const InputType = styled.h3`
-  padding-bottom: 10px;
+  padding: 10px 0;
   font-size: 15px;
   font-weight: 600;
 `;
@@ -21,20 +22,17 @@ const ClubContent = ({ onChange, initialContent }) => {
   const [content, setContent] = useState(initialContent);
 
   useEffect(() => {
-    setContent(initialContent);
-  }, [initialContent]);
+    if (JSON.stringify(content) !== JSON.stringify(initialContent)) {
+      onChange(content);
+    }
+  }, [content, initialContent]);
 
   const handleInputChange = (field) => (e) => {
     const newValue = e.target.value;
-    setContent((prev) => {
-      const newContent = {
-        ...prev,
-        [field]: newValue,
-      };
-
-      onChange(newContent);
-      return newContent;
-    });
+    setContent((prev) => ({
+      ...prev,
+      [field]: newValue,
+    }));
   };
 
   return (
@@ -50,42 +48,41 @@ const ClubContent = ({ onChange, initialContent }) => {
         </ListTextContainer>
       </ListDescriptionContainer>
       <InputContainer>
-        <div>
-          <InputType>소개</InputType>
-          <TextArea
-            height="139px"
-            value={content.introduce}
-            onChange={handleInputChange("introduce")}
-            placeholder="프로젝트 소개를 작성해주세요."
-          />
-        </div>
-        <div>
-          <InputType>목표</InputType>
-          <TextArea
-            height="70px"
-            value={content.goal}
-            onChange={handleInputChange("goal")}
-            placeholder="프로젝트의 최종 목표를 작성해주세요."
-          />
-        </div>
-        <div>
-          <InputType>계획</InputType>
-          <TextArea
-            height="217px"
-            value={content.plan}
-            onChange={handleInputChange("plan")}
-            placeholder="프로젝트 진행 계획에 대해 최대한 상세하게 작성해주세요."
-          />
-        </div>
-        <div>
-          <InputType>진행 형태</InputType>
-          <TextArea
-            height="84px"
-            value={content.participationMode}
-            onChange={handleInputChange("participationMode")}
-            placeholder={`프로젝트 진행 시 주로 활용할 진행 형태를 작성해주세요.\n(대면으로 진행 시, 주로 만날 지역/장소)`}
-          />
-        </div>
+        <InputContentContainer>
+          <div>
+            <InputType>소개</InputType>
+            <TextArea
+              height="139px"
+              value={content.introduce}
+              onChange={handleInputChange("introduce")}
+              placeholder="프로젝트 소개를 작성해주세요."
+            />
+
+            <InputType>목표</InputType>
+            <TextArea
+              height="70px"
+              value={content.goal}
+              onChange={handleInputChange("goal")}
+              placeholder="프로젝트의 최종 목표를 작성해주세요."
+            />
+
+            <InputType>계획</InputType>
+            <TextArea
+              height="217px"
+              value={content.plan}
+              onChange={handleInputChange("plan")}
+              placeholder="프로젝트 진행 계획에 대해 최대한 상세하게 작성해주세요."
+            />
+
+            <InputType>진행 형태</InputType>
+            <TextArea
+              height="84px"
+              value={content.participationMode}
+              onChange={handleInputChange("participationMode")}
+              placeholder={`프로젝트 진행 시 주로 활용할 진행 형태를 작성해주세요.\n(대면으로 진행 시, 주로 만날 지역/장소)`}
+            />
+          </div>
+        </InputContentContainer>
       </InputContainer>
     </FormListContainer>
   );
