4f319e69082718fa8a4655fdb90c981277187f4c
hienjoy <wisdom16no@naver.com>
2024-11-20 18:58:52 +0900

    feat: 프로젝트 등록, 확인 페이지 경로 추가

--- src/router.jsx
+++ src/router.jsx
@@ -4,28 +4,38 @@ import MainPage from "./pages/mainPage/mainPage";
 import ClubPage from "./pages/clubPage/ClubPage";
 import PaperPage from "./pages/paperPage/PaperPage";
 import MyPage from "./pages/myPage/MyPage";
+import ClubRegisterPage from "./pages/clubRegisterPage/ClubRegisterPage";
+import ClubRegisterDonePage from "./pages/clubRegisterPage/ClubRegisterDonePage";
 
 export default createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <OutletContainer />,
+    children: [
+      {
         path: "/",
-        element: <OutletContainer />,
-        children: [
-            {
-                path: "/",
-                element: <MainPage />
-            },
-            {
-                path: "/club",
-                element: <ClubPage />
-            },
-            {
-                path: "/paper",
-                element: <PaperPage />
-            },
-            {
-                path: "/my",
-                element: <MyPage />
-            }
-        ]
-    }
+        element: <MainPage />,
+      },
+      {
+        path: "/club",
+        element: <ClubPage />,
+      },
+      {
+        path: "/paper",
+        element: <PaperPage />,
+      },
+      {
+        path: "/my",
+        element: <MyPage />,
+      },
+      {
+        path: "/club/register",
+        element: <ClubRegisterPage />,
+      },
+      {
+        path: "/club/register/done",
+        element: <ClubRegisterDonePage />,
+      },
+    ],
+  },
 ]);
