b490500e8ebc95ada848b3a648147da7d42a70be
hienjoy <wisdom16no@naver.com>
2024-11-20 19:00:29 +0900

    feat(main): 프로젝트 등록 페이지로 이동 추가

--- src/pages/mainPage/components/postProject/PostProject.jsx
+++ src/pages/mainPage/components/postProject/PostProject.jsx
@@ -1,29 +1,32 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const PostProjectContinaer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: start;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: start;
+`;
 
 const Button = styled.button`
-    background-color: #00A8CC;
-    color: #000;
-    border: none;
-    border-radius: 10px;
-    padding: 10px 20px;
-`
-    
+  background-color: #00a8cc;
+  color: #000;
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+`;
 
 const PostProject = () => {
-    return (
-        <PostProjectContinaer>
-            <h2>같이 하고 싶은 사이드 프로젝트가 있나요?</h2>
-            <p>프로젝트 고민은 이제 그만!</p>
-            <Button>무료로 프로젝트 등록</Button>
-        </PostProjectContinaer>
-    );
+  const navigate = useNavigate();
+  return (
+    <PostProjectContinaer>
+      <h2>같이 하고 싶은 사이드 프로젝트가 있나요?</h2>
+      <p>프로젝트 고민은 이제 그만!</p>
+      <Button onClick={() => navigate("/club/register")}>
+        무료로 프로젝트 등록
+      </Button>
+    </PostProjectContinaer>
+  );
 };
 
 export default PostProject;
