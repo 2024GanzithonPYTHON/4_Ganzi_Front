b645e149e94444f345047cfb00bc3c85fd889af1
hienjoy <wisdom16no@naver.com>
2024-11-21 21:42:49 +0900

    feat: 헤더 로그인 후 닉네임 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.svg";
@@ -33,15 +33,27 @@ const ItemContainer = styled.div`
   gap: 30px;
 `;
 
-const LoginContainer = styled.div`
+const UserNameContainer = styled.div`
   display: inline-flex;
   align-items: center;
   gap: 8px;
 `;
 
+const LoginContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 10px;
+`;
+
 export function Header() {
   const navigate = useNavigate();
   const [isLoginned, setIsLoginned] = useState(false);
+
+  useEffect(() => {
+    if (!isLoginned) {
+      navigate("/");
+    }
+  }, [isLoginned, navigate]);
 
   const handleLogin = () => {
     setIsLoginned(!isLoginned);
@@ -73,7 +85,10 @@ export function Header() {
         <p onClick={toFindClub}>모집 찾기</p>
         <p onClick={toFindPaper}>비법서 찾기</p>
         <LoginContainer>
-          <img src={profileIcon} alt="로그인 아이콘" onClick={toMypage} />
+          <UserNameContainer>
+            <img src={profileIcon} alt="로그인 아이콘" onClick={toMypage} />
+            {isLoginned && <p onClick={toMypage}>개똥이님</p>}
+          </UserNameContainer>
           <p onClick={handleLogin}>{isLoginned ? "로그아웃" : "로그인"}</p>
         </LoginContainer>
       </ItemContainer>
