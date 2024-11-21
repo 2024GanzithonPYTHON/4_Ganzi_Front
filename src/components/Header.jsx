c5790db7869d1406c148c0f7987a09974c202b76
hienjoy <wisdom16no@naver.com>
2024-11-21 23:57:08 +0900

    fix: 로그인 상태에 따른 경로 변경 문제 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.svg";
@@ -49,16 +49,12 @@ export function Header() {
   const navigate = useNavigate();
   const [isLoginned, setIsLoginned] = useState(false);
 
-  useEffect(() => {
-    if (!isLoginned) {
+  const handleLogin = () => {
+    if (isLoginned) {
       navigate("/");
     }
-  }, [isLoginned, navigate]);
-
-  const handleLogin = () => {
     setIsLoginned(!isLoginned);
   };
-
   const toFindClub = () => {
     navigate("/club");
   };
