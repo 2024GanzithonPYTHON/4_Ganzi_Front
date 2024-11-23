04d53af0ca93f80c26cd9975669e5a46427f68e0
Chaeri-1 <alfsdo03@hufs.ac.kr>
2024-11-23 09:42:17 +0900

    userProfile

--- src/pages/myPage/components/userProfile/UserProfile.jsx
+++ src/pages/myPage/components/userProfile/UserProfile.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BalanceBox, BalanceContainer, PaySettingBox, SettingMenu, SettingMenuContainer, UserInfoContainer, UserName, UserProfileContainer, UserProfileImg, UserTextContainer } from './UserProfile.styled';
 import { FiSettings } from 'react-icons/fi';
+import { getUserInfo } from '../../../../server/user';
 
-const UserProfile = () => (
+const UserProfile = () => {
+
+    const userData = getUserInfo();
+
+    return (
     <UserProfileContainer>
         <UserInfoContainer>
             <UserProfileImg 
                 src="https://mblogthumb-phinf.pstatic.net/MjAyMTAzMTJfNjQg/MDAxNjE1NTI5NDE5ODk4.yxOKTxAf4mkrxbEWHopib3_XLI-EFI-7BCY91vi26TEg.NFAGxpj-t4T2hRS1dyoNrcC5DKUD4Y331ELpxJ5ghG8g.JPEG.sj330035/%EC%9D%B8%EC%8A%A4%ED%83%80%EA%B7%B8%EB%9E%A8_%ED%94%84%EC%82%AC_(1).jpeg?type=w800" 
                 alt="User Profile" />
             <UserTextContainer>
-                <UserName>이수혁님</UserName>
+                <UserName>{userData.name}님</UserName>
                 <p>cris4y</p>
             </UserTextContainer>
         </UserInfoContainer>
@@ -46,6 +51,7 @@ const UserProfile = () => (
             </SettingMenuContainer>
         </PaySettingBox>
     </UserProfileContainer>
-);
+    );
+};
 
 export default UserProfile;
