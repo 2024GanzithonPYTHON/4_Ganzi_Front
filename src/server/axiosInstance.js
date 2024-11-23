b3f552cd7350abfccfdc1a9db321f3d3d53090f0
ha <hcastle04@gamil.com>
2024-11-23 09:48:48 +0900

    fix: axios 링크 수정

--- src/server/axiosInstance.js
+++ src/server/axiosInstance.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://sidego.klr.kr",
-    // baseURL: "http://localhost:8080",
-    timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    withCredentials: true
+  baseURL: "https://api.sidego.klr.kr",
+  // baseURL: "http://localhost:8080",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export default axiosInstance;
