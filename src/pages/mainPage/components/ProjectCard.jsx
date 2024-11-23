212ec09cdc1722c7bacac0259ca2d17c5d06256d
hienjoy <wisdom16no@naver.com>
2024-11-23 14:21:21 +0900

    feat: 메인페이지 캐러셀 api 연동

--- src/pages/mainPage/components/ProjectCard.jsx
+++ src/pages/mainPage/components/ProjectCard.jsx
@@ -4,10 +4,21 @@ import {
   CardTitle,
   CardTitleText,
 } from "./ProjectCard.styled";
+import { useNavigate } from "react-router-dom";
 
-const ProjectCard = ({ title, src }) => {
+const ProjectCard = ({ type, id, title, src }) => {
+  const navigate = useNavigate();
+
+  const handleClub = () => {
+    navigate(`/club/detail/${id}`);
+  };
+
+  const handlePaper = () => {
+    navigate(`/paper/detail/${id}`);
+  };
+
   return (
-    <CardContainer>
+    <CardContainer onClick={type === "club" ? handleClub : handlePaper}>
       <CardImage src={src} alt={title} />
       <CardTitle>
         <CardTitleText>{title}</CardTitleText>
