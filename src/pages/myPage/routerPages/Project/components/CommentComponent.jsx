c222cfeea52573440f439f11672d8f0a0eb15c57
Chaeri-1 <alfsdo03@hufs.ac.kr>
2024-11-23 15:55:24 +0900

    feat: 지원서 정보 api

--- src/pages/myPage/routerPages/Project/components/CommentComponent.jsx
+++ src/pages/myPage/routerPages/Project/components/CommentComponent.jsx
@@ -1,9 +1,24 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { BtnContainer, CommentBox, CommentBoxTitle, CommentBtn, CommentContentBox, CommentModal, CommentModalBox, CommentModalProfileBox, CommentProfileBox, CommentWrapper, DateBox, ModalBtn, ModalBtnBox, MoreInfoLinkBtn } from './Toggle.styled'
 import mail from '../assets/Letter-icon.svg'
+import { getApplicant } from '../../../../../server/applicant';
 
 export default function CommentComponent({item}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [applyData, setApplyData] = useState({
+    id: 1,
+    shortTitle: "귀여운 강아지 캐릭터를 만들고 캐릭터로 스티커, 키링 등 굿즈를 만들 팀원 구합니다!",
+    username: "b",
+    email: "b@gmail.com",
+    content: "평소에 캐릭터 만들기를 좋아하고 종종 해왔습니다. 이번 기회에 캐릭터를 이용한 다양한 굿즈를 제작해보고 싶습니다.",
+    career: "마스코트 제작 수상 경력",
+    createdAt: "2024-11-20"
+  });
+
+  useEffect(() => {
+    const userId = item.applicantId
+    getApplicant().then((response) => setApplyData(response));
+  }, [userId])
 
   const handleAcceptBtn = () => {
     const isAccepted = window.confirm(`${item.userId}의 프로젝트 참여 신청을 수락하겠습니까?`)
@@ -42,7 +57,9 @@ export default function CommentComponent({item}) {
           <span>{item.content.length > 35
           ? `${item.content.slice(0, 35)}...`
           : item.content}</span>
-          <MoreInfoLinkBtn onClick={openModal}>더보기</MoreInfoLinkBtn>
+          <MoreInfoLinkBtn
+          onClick={openModal}
+          >더보기</MoreInfoLinkBtn>
         </CommentBox>
         <BtnContainer>
           <CommentBtn onClick={() => handleAcceptBtn ()} color='#529F52'>수락</CommentBtn>
@@ -52,13 +69,13 @@ export default function CommentComponent({item}) {
       {isModalOpen && (
         <CommentModal onClick={closeModal}>
           <CommentModalBox onClick={(e) => e.stopPropagation()}>
-            <CommentBoxTitle><span>⌜대학생 할러윈 파티 기획하기⌟</span>의 지원자</CommentBoxTitle>
-            <DateBox><span>2024.10.11</span>지원</DateBox>
+            <CommentBoxTitle><span>{applyData.shortTitle}</span>의 지원자</CommentBoxTitle>
+            <DateBox><span>{applyData.createdAt}</span>지원</DateBox>
             <CommentModalProfileBox>
               <img className='profile'/>
               <div className='profile-text-box'>
                 <p className='profile-text'><span>{item.userId}</span> 20대 여</p>
-                <span className='mail-box'><img src={mail}/>crispy@ganzi.com</span>
+                <span className='mail-box'><img src={mail}/>{applyData.email}</span>
               </div>
               <ModalBtnBox>
                 <ModalBtn onClick={() => handleAcceptBtn ()} color='#529F52'>수락</ModalBtn>
@@ -67,9 +84,9 @@ export default function CommentComponent({item}) {
             </CommentModalProfileBox>
             <CommentContentBox>
               <p>지원 동기</p>
-              <textarea>{item.comment}</textarea>
+              <textarea>{applyData.content}</textarea>
               <p>관련 경력</p>
-              <textarea>{item.resume}</textarea>
+              <textarea>{applyData.career}</textarea>
             </CommentContentBox>
           </CommentModalBox>
         </CommentModal>
