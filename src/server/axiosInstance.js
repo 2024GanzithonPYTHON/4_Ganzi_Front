9e30258ba8009f5fc7aa39006c24869ad88da4a3
leestana01 <leestana01@naver.com>
2024-11-23 10:08:56 +0900

    fix: api 경로 수정

--- src/server/axiosInstance.js
+++ src/server/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "https://sidego.klr.kr",
+    baseURL: "https://api.sidego.klr.kr",
     // baseURL: "http://localhost:8080",
     timeout: 5000,
     headers: {
